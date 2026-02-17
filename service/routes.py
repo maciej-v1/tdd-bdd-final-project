82faa2b77c598df2eefdf622a50d49a7822ac984
maciej-v1 <maciej.modzelewski@version1.com>
2026-02-17 16:11:05 +0000

    Update routes.py for Exercise 3

--- service/routes.py
+++ service/routes.py
@@ -106,9 +106,22 @@ def create_products():
 # R E A D   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_product(product_id):
+    """
+    Retrieve a single Product by its ID.
+    """
+    app.logger.info("Request to retrieve product with id [%s]", product_id)
+
+    product = Product.find(product_id)
+    if product is None:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Product with id '{product_id}' was not found."
+        )
+
+    app.logger.info("Product found: %s", product.name)
+    return product.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
